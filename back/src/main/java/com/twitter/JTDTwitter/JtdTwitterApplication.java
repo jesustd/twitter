db73e2095323459c65970a800a0dea7ef9282fee
yisusss <7836095@alumnos.ufv.es>
2023-06-30 12:36:52 +0200

    correccion de errores y comprobacion

--- back/src/main/java/com/twitter/JTDTwitter/JtdTwitterApplication.java
+++ back/src/main/java/com/twitter/JTDTwitter/JtdTwitterApplication.java
@@ -3,10 +3,20 @@ package com.twitter.JTDTwitter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class JtdTwitterApplication {
 
 	public static void main(String[] args) {
+
+		ArrayList<Tweet> Datas = new ArrayList<>();
+		LectorJson lector = new LectorJson();
+		Datas = lector.LecturaJSONsimple();
+
+		System.out.println(Datas);
+
 		SpringApplication.run(JtdTwitterApplication.class, args);
 	}
 
