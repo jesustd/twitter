a118beaa1c8dfaf5a24b9d1c77e7c8e3803368fc
yisusss <7836095@alumnos.ufv.es>
2023-06-30 11:44:13 +0200

    scafolding del proyecto back

---
+++ back/src/main/java/com/twitter/JTDTwitter/JtdTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.JTDTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtdTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtdTwitterApplication.class, args);
+	}
+
+}
