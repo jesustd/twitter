880bce7a09c38749aa608bc21069db377f2ccb64
yisusss <7836095@alumnos.ufv.es>
2023-07-01 03:22:32 +0200

    finalizacion de post get put y delete

--- front/src/main/java/org/vaadin/example/MainView.java
+++ front/src/main/java/org/vaadin/example/MainView.java
@@ -12,12 +12,15 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 /**
@@ -41,7 +44,7 @@ import java.util.ArrayList;
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout {
 
-    private static final String API_URL = "http://localhost:8085";
+
 
     //HttpClient client = HttpClient.newBuilder().build();
     HttpClient client = HttpClient.newBuilder().build();
@@ -60,11 +63,109 @@ public class MainView extends VerticalLayout {
 
     Dialog dialog =new Dialog();
 
+    private static final String api = "http://localhost:8085";
+
+    public Tweet TweetApi(){
+
+        Tweet TweetJSON;
 
 
+        String StringTweet = getData("Tweet", null);
+
+        TweetJSON = gson.fromJson(StringTweet, Tweet.class);
+
+        return TweetJSON;
+    }
+
+    private void postData(String url1, String url2, Tweet AuxTweet) {
+        try {
+
+            String resource;
+
+            if (url2 == null){
+
+                resource = String.format(api + url1);
+
+                request = HttpRequest
+                        .newBuilder(new URI(resource))
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(""))
+                        .build();
+
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            }
+            else{
+
+                resource = String.format(api + url1 + "/" + url2);
+
+                request = HttpRequest
+                        .newBuilder(new URI(resource))
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(AuxTweet.toString()))
+                        .build();
+
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            }
+
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
+    private String getData(String url1, String url2){
+        try {
+            String resource;
 
+            if (url2 == null){
+                resource = String.format(api + url1);
+            }
+            else{
+                resource = String.format(api + url1 + "/" + url2);
+            }
+
+            request= HttpRequest
+                    .newBuilder(new URI(resource))
+                    .header("Content-Type","application/json")
+                    .GET()
+                    .build();
+            response=client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return (String) response.body();
+    }
+
+    private void putData(String url1, String url2, int Id, Tweet tweets){
+        try {
+            String resource = String.format(api + url1 + "/" + url2 + "/" + Id);
+            request= HttpRequest
+                    .newBuilder(new URI(resource))
+                    .header("Content-Type","application/json")
+                    .PUT(HttpRequest.BodyPublishers.ofString(tweets.toString()))
+                    .build();
+            response=client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteData(String url1, String url2, int Id, Tweet tweets){
+        try {
+            String resource = String.format(api + url1 + "/" + url2 + "/" + Id);
+            request= HttpRequest
+                    .newBuilder(new URI(resource))
+                    .header("Content-Type","application/json")
+                    .DELETE()
+                    .build();
+            response=client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * Construct a new Vaadin view.
      * <p>
